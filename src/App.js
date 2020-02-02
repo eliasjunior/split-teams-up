2a2fe6c57d56779625cdf10bd84161a01fa00276
Elias Junior <emcjunior@gmail.com>
2020-02-02 23:41:00 +0000

    fix border

--- src/App.js
+++ src/App.js
@@ -3,49 +3,24 @@ import "./App.css";
 import Teams from "./teams-app";
 import Header from "./teams-app/components/header";
 import Example from "./teams-app/components/example";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-      <Router>
-        <div>
-          {/* <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/example">Example</Link>
-              </li>
-            </ul>
-          </nav> */}
-  
-          {/* A <Switch> looks through its children <Route>s and
-              renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/example">
-              <div  className="App">
-                <Header></Header>
-                <Example></Example>
-              </div>
-            </Route>
-            <Route path="/">
-              <div  className="App">
-                <Header></Header>
-                <Teams></Teams>
-              </div>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/example">
+            <Example />
+          </Route>
+          <Route path="/">
+            <Teams />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
 
 export default App;
